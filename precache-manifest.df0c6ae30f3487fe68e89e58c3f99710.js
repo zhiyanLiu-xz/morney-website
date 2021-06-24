04b1dfa0a81241be7e1a04c160bc1c4c1b51c1b5
刘芷言 <liuzhiyan138@163.com>
2021-06-24 11:41:42 +0800

    deploy

---
+++ precache-manifest.df0c6ae30f3487fe68e89e58c3f99710.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52391a39bcf74c519b08",
+    "url": "/morney-website/css/app.8564e603.css"
+  },
+  {
+    "revision": "1f530912bee5fa5d106a",
+    "url": "/morney-website/css/chunk-vendors.def3fd65.css"
+  },
+  {
+    "revision": "f47219e6ab82632f881bcb7faadbc9e3",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "52391a39bcf74c519b08",
+    "url": "/morney-website/js/app.3f0d2642.js"
+  },
+  {
+    "revision": "1f530912bee5fa5d106a",
+    "url": "/morney-website/js/chunk-vendors.fcc0d9f8.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "fc8879a2208785396dbb4321fe400b4d",
+    "url": "/morney-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
